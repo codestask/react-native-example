9184d9d873f0eb38252bf7c396e05c44dd8b8f88
codestask <codestask.glo@gmail.com>
2021-02-09 23:18:41 +0700

    Init Project

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
